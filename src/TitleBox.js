9c157f452b356057de191921e470a9c53d93a27f
hadleydalt <77938899+hadleydalt@users.noreply.github.com>
2023-12-22 21:01:26 -0700

    hi

--- src/TitleBox.js
+++ src/TitleBox.js
@@ -41,33 +41,3 @@ export const TitleBox = (props) => {
         </div>
     )
 }
-
-
-/*
-
-<div className="Title-box-box" style={{marginTop: "-0.8vh", marginLeft: "-0.5vw"}} />
-                <div className="Title-box-box" style={{marginTop: "-0.8vh", marginLeft: "6.5vw"}} />
-                <div className="Title-box-box" style={{marginTop: "-0.8vh", marginLeft: "13.4vw"}} />
-                <div className="Title-box-box" style={{marginTop: "9.5vh", marginLeft: "-0.5vw"}} />
-                <div className="Title-box-box" style={{marginTop: "9.5vh", marginLeft: "6.5vw"}} />
-                <div className="Title-box-box" style={{marginTop: "9.5vh", marginLeft: "13.4vw"}} />
-
-
-                <div className="Title-box-text">
-            <div className="Title-box-circle" style={{marginTop: "2vh", marginLeft: "-0.4vw"}}>
-                <div className="Title-box-inner-circle" />
-            </div>
-            <div className="Title-box-circle" style={{marginTop: "6.5vh", marginLeft: "-0.4vw"}}>
-                <div className="Title-box-inner-circle" />
-            </div>
-            <div className="Title-box-circle" style={{marginTop: "2vh", marginLeft: "13.5vw"}}>
-                <div className="Title-box-inner-circle" />
-            </div>
-            <div className="Title-box-circle" style={{marginTop: "6.5vh", marginLeft: "13.5vw"}}>
-                <div className="Title-box-inner-circle" />
-                
-            </div>
-                Hello!
-            </div>
-
-            */
