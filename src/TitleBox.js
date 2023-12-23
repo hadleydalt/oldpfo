fada21805a39b5872c664ff60a6f9e10b9c5b8bb
hadleydalt <77938899+hadleydalt@users.noreply.github.com>
2023-12-22 20:36:29 -0700

    hi

--- src/TitleBox.js
+++ src/TitleBox.js
@@ -5,28 +5,69 @@ export const TitleBox = (props) => {
 
     return (
         <div className="Title-box-wrapper">
-            <div className="Title-box-box" style={{marginTop: -3, marginLeft: -3}} />
-            <div className="Title-box-box" style={{marginTop: -3, marginLeft: 163}} />
-            <div className="Title-box-box" style={{marginTop: -3, marginLeft: 337}} />
-            <div className="Title-box-box" style={{marginTop: 67, marginLeft: -3}} />
-            <div className="Title-box-box" style={{marginTop: 67, marginLeft: 163}} />
-            <div className="Title-box-box" style={{marginTop: 67, marginLeft: 337}} />
-
-            <div className="Title-box-circle" style={{marginTop: 10, marginLeft: 10}}>
-                <div className="Title-box-inner-circle" />
-            </div>
-            <div className="Title-box-circle" style={{marginTop: 50, marginLeft: 10}}>
-                <div className="Title-box-inner-circle" />
-            </div>
-            <div className="Title-box-circle" style={{marginTop: 10, marginLeft: 318}}>
-                <div className="Title-box-inner-circle" />
-            </div>
-            <div className="Title-box-circle" style={{marginTop: 50, marginLeft: 318}}>
-                <div className="Title-box-inner-circle" />
-            </div>
             <div className="Title-box">
-                Hello my name is...
+                <div className="Title-box-border"></div>
+                <div className="Title-box-text">
+                    Hello!
+                </div>
+                <div className="Title-box-box-row">
+                    <div className="Title-box-box" />
+                    <div className="Title-box-box" />
+                    <div className="Title-box-box" />
+                </div>
+                <div className="Title-box-circle-row">
+                    <div className="Title-box-circle">
+                        <div className="Title-box-inner-circle" />
+                    </div>
+                    <div className="Title-box-circle">
+                        <div className="Title-box-inner-circle" />
+                    </div>
+                </div>
+                <div className="Title-box-spacer" />
+                <div className="Title-box-circle-row">
+                    <div className="Title-box-circle">
+                        <div className="Title-box-inner-circle" />
+                    </div>
+                    <div className="Title-box-circle">
+                        <div className="Title-box-inner-circle" />
+                    </div>
+                </div>
+                <div className="Title-box-box-row">
+                    <div className="Title-box-box" />
+                    <div className="Title-box-box" />
+                    <div className="Title-box-box" />
+                </div>
             </div>
         </div>
     )
 }
+
+
+/*
+
+<div className="Title-box-box" style={{marginTop: "-0.8vh", marginLeft: "-0.5vw"}} />
+                <div className="Title-box-box" style={{marginTop: "-0.8vh", marginLeft: "6.5vw"}} />
+                <div className="Title-box-box" style={{marginTop: "-0.8vh", marginLeft: "13.4vw"}} />
+                <div className="Title-box-box" style={{marginTop: "9.5vh", marginLeft: "-0.5vw"}} />
+                <div className="Title-box-box" style={{marginTop: "9.5vh", marginLeft: "6.5vw"}} />
+                <div className="Title-box-box" style={{marginTop: "9.5vh", marginLeft: "13.4vw"}} />
+
+
+                <div className="Title-box-text">
+            <div className="Title-box-circle" style={{marginTop: "2vh", marginLeft: "-0.4vw"}}>
+                <div className="Title-box-inner-circle" />
+            </div>
+            <div className="Title-box-circle" style={{marginTop: "6.5vh", marginLeft: "-0.4vw"}}>
+                <div className="Title-box-inner-circle" />
+            </div>
+            <div className="Title-box-circle" style={{marginTop: "2vh", marginLeft: "13.5vw"}}>
+                <div className="Title-box-inner-circle" />
+            </div>
+            <div className="Title-box-circle" style={{marginTop: "6.5vh", marginLeft: "13.5vw"}}>
+                <div className="Title-box-inner-circle" />
+                
+            </div>
+                Hello!
+            </div>
+
+            */
