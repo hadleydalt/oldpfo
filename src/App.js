068a831d8061318720fb859b96bfa0af749ee441
hadleydalt <77938899+hadleydalt@users.noreply.github.com>
2023-12-26 01:20:53 -0700

    hi

--- src/App.js
+++ src/App.js
@@ -65,8 +65,8 @@ function App() {
           <TitleBox expanded={width < 877} />
           <div className="sidebar-desc">
             <div className="serif">I'm Hadley.</div>
-            <div className="small">A cross-disciplinary developer, experimental aestheticist, and lover of the details. My work is an attempt to bind the details together.</div>
-            <div className="small">I'm hoping to make an impact and use my explorations to advance medtech, facilitate communication, and generally galvanize people.</div>
+            <div className="small">A cross-disciplinary developer, experimental aestheticist, and lover of the details. My projects are attempts to bind the details together.</div>
+            <div className="small">I'm hoping to do so in meaningful ways that make a difference in people's lives!</div>
             <div className="small-serif">Want to work together? Let me know at <b>hadley_dalton@brown.edu</b></div>
           </div>
           <div className="bottom-bar">
