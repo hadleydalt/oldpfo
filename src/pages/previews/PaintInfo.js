7a7a350619ed369448b6b0727b3e9d1c1d088a59
hadleydalt <77938899+hadleydalt@users.noreply.github.com>
2024-01-04 21:03:57 -0700

    hi

---
+++ src/pages/previews/PaintInfo.js
@@ -0,0 +1,43 @@
+
+export const Dates = ["FEBRUARY 2020", "DECEMBER 2018", "APRIL 2020", "JANUARY 2020", "MAY 2020", "OCTOBER 2018", "FEBRUARY 2020", "JULY 2018", "JANUARY 2020", "OCTOBER 2019", "NOVEMBER 2019"]
+export const Titles = ["Shell Explosion", "Victory", "Lesson On Chalkboard", "Candyland", "I See The Light", "Reflections", "Transient Purity", "Train of Thought", "Hidden Faces", "Let It Sink In", "Kowloon Walled City"]
+export const ClassInfo = [
+    "Foundations Drawing with Ellie Hollinshead",
+    "Independent Project",
+    "Foundations Drawing with Ellie Hollinshead Jason Brockert",
+    "Painting Marathon with Claudia Bitran",
+    "Foundations Drawing with Ellie Hollinshead and Jason Brockert",
+    "Independent Project",
+    "Foundations Design with Leslie Hirst",
+    "Independent Project",
+    "Painting Marathon with Michael Peery",
+    "Foundations Design with Yvgenia Baras",
+    "Foundations Drawing with Jonathan Sylvia"
+]
+export const Size = [
+    "Approx. 4x4 feet",
+    "18x24 inches",
+    "18x18 inches",
+    "20x20 inches",
+    "3x4 feet",
+    "18x24 inches",
+    "Approx. 3x5x2 feet"
+]
+export const Media = [
+    "Blender, Unity, VRChat",
+    "React, TypeScript, SCSS, MongoDB",
+    "React Native, JavaScript, Docker",
+    "Java, JavaFX",
+    "Figma",
+    "Figma, React, JavaScript",
+    "C++, OpenGL"
+]
+export const Info = [
+    "Cat Finder is an adventure game with the objective of offering users a refuge from the stress of everyday life. Modelled from scratch in Blender and programmed in Unity, the game can be played on PC or in VR!",
+    "I was challenged to code an IDE for document-based networks using TypeScript, React, and SCSS. I used this challenge as an opportunity to propose some suggestions for a hypothetical redesign of the existing web app.",
+    "Soma is an app which allows users to track and monitor chronic pain. I coded responsive components and offered design suggestions that would facilitate user navigation.",
+    "I created a game which allows users to track their productivity and earn pets in exchange. The goal of the game is to offer users an incentive to study! This was my first independent programming project.",
+    "A design for the pilot website of Boston AI Labs: a startup company that designs AI technology to detect and combat Retinopathy of Prematurity in newborn infants.",
+    "I architected and executed responsive web solutions, and also redesigned existing web pages with mindfulness of UX principles. I also conducted user testing.",
+    "I created a ray tracer which parses tree-based scene representations, projects a ray to calculate intersection points, and uses a Phong lighting model to color each pixel of an image."
+]
