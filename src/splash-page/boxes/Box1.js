61a89343b20e020aa2ba61bcc075bc79659fa6e7
hadleydalt <77938899+hadleydalt@users.noreply.github.com>
2024-01-06 20:24:21 -0700

    hi

--- src/splash-page/boxes/Box1.js
+++ src/splash-page/boxes/Box1.js
@@ -22,8 +22,8 @@ export const Box1 = (props) => {
                 <div className="Box1-interior" style={{height: String((height - 50) + "px")}}>
                     <img src={require('../../graphics/Art2.png')} alt="" className="Box1-image" style={{height: String((height - 105) + "px")}} draggable={false} />
                     <div className="Box1-button-wrapper">
-                        <Button name="About" width={100} height={30} handleSwitch={() => {props.handleSwitch(104, true, false)}} />
-                        <Button name="Resume" width={100} height={30}/>
+                        <Button name="About" width={100} height={30} link={false} nav={104} handleSwitch={props.handleSwitch} />
+                        <Button name="Resume" width={100} height={30} link={true} nav="https://drive.google.com/file/d/1nhlNgpAaA78G_4ht7_sIVIxKiRovm8jV/view?usp=sharing"  />
                     </div>
                 </div>
             </div>
--- src/splash-page/boxes/Button.js
+++ src/splash-page/boxes/Button.js
@@ -2,6 +2,9 @@ import '../../css/App.css'
 import { useState } from 'react'
 
 export const Button = (props) => {
+    const link = props.link
+    const nav = props.nav
+
     const [isMouseEntered, setIsMouseEntered] = useState(false)
     const [isPressed, setIsPressed] = useState(false)
 
@@ -29,13 +32,35 @@ export const Button = (props) => {
                 width: props.width + 12
             }}
         >
+            {link && <a href={nav} style={{textDecoration: "none"}}>
             <div 
                 className="Box1-button"
                 onMouseDown={handleMouseDown}
                 onMouseUp={handleMouseUp}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
-                onClick={props.handleSwitch}
+                style={{
+                    cursor: isMouseEntered ? "pointer" : "default",
+                    borderBottom: isPressed ? "none" : "4px solid gray",
+                    borderRight: isPressed ? "none" : "4px solid lightgray",
+                    borderTop: isPressed ? "none" : "4px solid #fafafa",
+                    borderLeft: isPressed ? "none" : "4px solid #E7E7E7",
+                    height: isPressed ? props.height + 8 : props.height,
+                    width: isPressed ? props.width + 8 : props.width,
+                    filter: isPressed ? "drop-shadow(1px 1px 5px rgba(0, 0, 0, 0.5)" : "none",
+                }}
+            >
+                {props.name}
+            </div>
+                </a>}
+            {!link && 
+            <div 
+                className="Box1-button"
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onClick={() => {props.handleSwitch(nav, true, false)}}
                 style={{
                     cursor: isMouseEntered ? "pointer" : "default",
                     borderBottom: isPressed ? "none" : "4px solid gray",
@@ -48,7 +73,7 @@ export const Button = (props) => {
                 }}
             >
                 {props.name}
-            </div>
+            </div>}
         </div>
     )
 }
